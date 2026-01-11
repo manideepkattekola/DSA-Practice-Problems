7ef5db069101382b5ee05a7eaab512702c715ee2
Manideep <163756659+manideepkattekola@users.noreply.github.com>
2026-01-11 14:38:26 +0530

    Add files via upload

---
+++ Program 2.py
@@ -0,0 +1,4 @@
+a = int(input("Enter num1: "))
+b = int(input("Enter num2: "))
+sum = a + b
+print("Sum =",sum)
---
+++ Program 3.py
@@ -0,0 +1,6 @@
+num = int(input("Enter a num: "))
+
+if num % 2 == 0:
+    print("The num is Even")
+else:
+    print("The num is odd")
